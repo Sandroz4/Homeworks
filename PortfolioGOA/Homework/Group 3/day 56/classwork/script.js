00d50c1939ad38b9234d590571485e61acdfce04
Sandroz4 <sandrozabakhidze888@gmail.com>
2024-01-29 23:00:32 +0400

    commit

---
+++ PortfolioGOA/Homework/Group 3/day 56/classwork/script.js
@@ -0,0 +1,57 @@
+// let num = 5
+
+// console.log(5 == "5"); //doesnt compare everything
+// console.log(5 === 5); //compares everything
+
+
+// let EnterGoa = confirm("Do you want to Join Goa?");
+
+// if (EnterGoa) {
+//     let userAge = prompt("Please enter your age:");
+
+//     if (userAge >= 18) {
+//         alert("Welcome to Group 4!");
+//     } else {
+//         alert("Welcome to Group 3!");
+//     }
+// } else {
+//     alert("So bad!.");
+// }
+
+// let joinGoa = false;
+
+// while (!joinGoa) {
+//     let answer = prompt("Do you want to join Goa? (yes/no)");
+
+//     if (answer.toLowerCase() === 'yes') {
+//         joinGoa = true;
+//         console.log("Great choice! time to escape the Matrix.");
+//     } else {
+//         console.log("So bad, Matrix has you!.");
+//     }
+// } 
+
+// for (let i = 1; i <= 1000; i++) {
+//     if (i === 1000) {
+//         console.log(`${i} You know what, I don't anymore`);
+//     } else {
+//         console.log(`${i} I love you`);
+//     }
+// }
+
+
+// let userInput = prompt("Enter a number:");
+// let number = parseFloat(userInput);
+// let negativeNumber = -number;
+
+// console.log(`The negative of ${number} is: ${negativeNumber}`);
+
+
+// let numbers = [5, 10, 15, 20, 25];
+// let sum = 0;
+
+// for (let i = 0; i < numbers.length; i++) {
+//   sum += numbers[i];
+// }
+// let avg = sum / numbers.length;
+
