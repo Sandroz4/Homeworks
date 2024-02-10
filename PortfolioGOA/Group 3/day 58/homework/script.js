5bbe183f39fdd4de4fb6d5abda2985eee89fa856
Sandroz4 <sandrozabakhidze888@gmail.com>
2024-02-11 02:38:32 +0400

    commit

---
+++ PortfolioGOA/Group 3/day 58/homework/script.js
@@ -0,0 +1,33 @@
+// script.js
+function userInfo(Name, Email, Password) {
+    this.name = Name;
+    this.email = Email;
+    this.password = Password;
+  }
+  
+  document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('submitButton').addEventListener('click', function () {
+      submitForm();
+    });
+  });
+  
+  function submitForm() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+  
+    var userInfo1 = new userInfo(name, email, password);
+  
+    displayUserInfo(userInfo1);
+  }
+  
+  function displayUserInfo(userInfo) {
+    var userInfoDisplay = document.getElementById('userInfoDisplay');
+
+    var userInfoHTML = '<h3>User Information:</h3>';
+    userInfoHTML += '<p><strong>Name:</strong> ' + userInfo.name + '</p>';
+    userInfoHTML += '<p><strong>Email:</strong> ' + userInfo.email + '</p>';
+    userInfoHTML += '<p><strong>Password:</strong> ' + userInfo.password + '</p>';
+
+    userInfoDisplay.innerHTML = userInfoHTML;
+  }
