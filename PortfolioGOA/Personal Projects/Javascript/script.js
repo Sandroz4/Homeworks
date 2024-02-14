48f057b69699163d6305e8d2887170e02168ff97
Sandroz4 <sandrozabakhidze888@gmail.com>
2024-02-15 00:50:28 +0400

    commit

---
+++ PortfolioGOA/Personal Projects/Javascript/script.js
@@ -0,0 +1,80 @@
+//variables
+// let x;
+// x = 123;
+
+// console.log(x)
+
+// let age = 25;
+// let price = 10.99;
+// let gpa = 2.1;
+
+// console.log('you are ${age} years old');
+// console.log('the price is ${price}')
+// console.log(typeof age)
+
+
+// let firstName = 'Sandro';
+
+//types
+// console.log(typeof firstName)
+// console.log(firstName)
+
+// let students = 30;
+// students += 1;
+// students -= 1;
+// students *= students;
+// students /= students;
+// students = students ** 3;
+// students = students % 2;
+
+// console.log(students);
+
+//user input
+// let username;
+
+// username = window.prompt('whats ur username');
+
+// console.log(username);
+
+// let username;
+
+// document.getElementById("mySubmit").onclick = function(){
+//     username = document.getElementById("myText").value;
+//     document.getElementById("myH1").textcontent = 'hello ${username}'
+// }
+
+
+// let age = window.prompt("How old are you")
+// age = Number(age);
+// age+=1;
+// console.log(age, typeof age);
+
+//type conversion
+// let x = '0';
+// let y = '0';
+// let z = '0';
+
+// x = Number(x);
+// y = String(y);
+// z = Boolean(z);
+
+// console.log(x, typeof x);
+// console.log(y, typeof y);
+// console.log(z, typeof z);
+
+
+//constants
+
+// const =  a variable that can't be changed
+
+// const PI = 3.14159;
+// let radius;
+// let circumference;
+
+// radius = window.prompt('Enter the radius of a cirlce');
+// radius = Number(radius);
+
+// circumference = 2 * pi * radius;
+
+// console.log(circumference)
+
