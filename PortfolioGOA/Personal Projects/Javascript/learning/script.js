0e179f6bb26ec081bcd2067f3467d61718023f89
Sandroz4 <sandrozabakhidze888@gmail.com>
2024-02-15 23:42:57 +0400

    commit:

--- PortfolioGOA/Personal Projects/Javascript/learning/script.js
+++ PortfolioGOA/Personal Projects/Javascript/learning/script.js
@@ -78,3 +78,28 @@
 
 // console.log(circumference)
 
+//Math
+// Math.PI
+// Math.E
+
+// let x = 3.21;
+// let y = 2;
+// let z;
+
+// z = Math.round(x);
+// z = Math.floor(x);
+// z = Math.ceil(x);
+// z = Math.trunc(x);
+// z = Math.pow(x);
+// z = Math.sqrt(x);
+// z = Math.floor(x);
+// z = Math.sin(x);
+// z = Math.cos(x);
+// z = Math.tan(x);
+// z = Math.abs(x);
+// z = Math.log(x);
+// z = Math.sign(x);
+// let max = Math.max(x, y, z)
+// let min = Math.min(x, y, z)
+
+// console.log(max)
