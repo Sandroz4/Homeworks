6b8600a1b1ca58517005cb73a45e05b21961a10f
Sandroz4 <sandrozabakhidze888@gmail.com>
2024-02-17 15:30:19 +0400

    commit

--- PortfolioGOA/Personal Projects/Javascript/learning/script.js
+++ PortfolioGOA/Personal Projects/Javascript/learning/script.js
@@ -103,3 +103,105 @@
 // let min = Math.min(x, y, z)
 
 // console.log(max)
+
+
+//math random
+// const min = 50;
+// const max = 100;
+
+// let randomNum = Math.floor(Math.random() * (max - min)) + min;
+
+// console.log(randomNum);
+
+// const myButton = document.getElementById('myButton');
+// const label1 = document.getElementById('label1');
+// const label2 = document.getElementById('label2');
+// const label3 = document.getElementById('label3');
+// const min = 1;
+// const max = 6;
+// let randomNum1;
+// let randomNum2;
+// let randomNum3;
+
+// myButton.onclick = function(){
+//     randomNum1 = Math.floor(Math.random() * max) + min;
+//     label1.textContent = randomNum1;
+//     randomNum2 = Math.floor(Math.random() * max) + min;
+//     label2.textContent = randomNum2;
+//     randomNum3 = Math.floor(Math.random() * max) + min;
+//     label3.textContent = randomNum3;
+// }
+
+// let age = 120;
+
+// if(age >= 18){
+//     console.log('you are old')
+// }
+// else{
+//     console.log('you are young')
+// }
+
+// let time = 14;
+
+// if(time < 12){
+//     console.log('good morning')
+// }
+// else(
+//     console.log('good afternoon')
+// )
+
+// let isStudent = false;
+
+// if(isStudent){
+//     console.log('you are a student')
+// }
+// else{
+//     console.log('you are not a dtudent!')
+// }
+
+// let isStudent = true;
+
+// if(isStudent){
+//     console.log('you are a student')
+// }
+// else{
+//     console.log('you are not a dtudent!')
+// }
+
+// let age = 16;
+// let hasLicense = false;
+
+// if(age >= 16){
+//     console.log('you are old enough to drive')
+
+//     if(hasLicense){
+//         console.log('you have a license')
+//     }
+//     else{
+//         console.log('you do not have your licesne yet')
+//     }
+// }
+// else{
+//     console.log('you must be 16+ to have a drivers license')
+// }
+
+// let age = 10;
+
+
+// if(age >= 100){
+//     console.log('you are too old to enter the site')
+// }
+// else if(age == 0){
+//     console.log('you cant enter, you were just born')
+// }
+// else if(age >= 18){
+//     console.log('you are old enough to enter this site')
+// }
+// else if(age < 0){
+//     console.log('your age cant be below 0')
+// }
+// else{
+//     console.log('you must be 18+')
+// }
+
+  
