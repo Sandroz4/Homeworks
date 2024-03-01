52606815d090a4b34a919a3505cc40817337b0a5
Sandroz4 <sandrozabakhidze888@gmail.com>
2024-03-02 00:19:09 +0400

    commit

--- Goal Oriented Academy/Group 5/day 07/homewrok.py
+++ Goal Oriented Academy/Group 5/day 07/homewrok.py
@@ -1,18 +1,6 @@
-#defining ticket_price variable
-ticket_price = 0
-#defining age_count variable
-people_count = 0
+num1 = int(input("enter a number:"))
+num2 = int(input("enter a number:"))
 
-#creating a while loop
-while people_count < 13:
-  age = (int(input("Enter age: ")))
-  if age <= 18:
-    ticket_price += 0
-    people_count += 1
-  else:
-    ticket_price += 25
-    people_count += 1
+comparison = num1 == num2
 
-#printing ticket_price
-print(ticket_price)
-print(people_count)
+print(comparison)
