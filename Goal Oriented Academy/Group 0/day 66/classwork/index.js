43635a9ef05fc88b8f469d27ea677d861caf3b0f
Sandroz4 <sandrozabakhidze888@gmail.com>
2024-03-08 00:00:13 +0400

    commit

---
+++ Goal Oriented Academy/Group 0/day 66/classwork/index.js
@@ -0,0 +1,31 @@
+// let numbers = []
+
+// for (let i = 0; i < 10; i++) {
+//     let input = prompt(`Enter number ${i + 1}:`)
+//     numbers.push(parseFloat(input))
+// }
+
+// let sum = 0
+// for (let num of numbers) {
+//     sum += num
+// }
+
+// let product = 1
+// for (let num of numbers) {
+//     product *= num
+// }
+
+// let count = numbers.length
+
+// let squareRoots = []
+// for (let num of numbers) {
+//     squareRoots.push(Math.sqrt(num))
+// }
+
+// console.log("Numbers:", numbers)
+// console.log("Sum:", sum)
+// console.log("Product:", product)
+// console.log("Count:", count)
+// console.log("Square roots:", squareRoots)
+
+
