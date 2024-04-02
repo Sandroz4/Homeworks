b90e26312113e296bfc3af7f4d48793578f94329
Sandroz4 <sandrozabakhidze888@gmail.com>
2024-04-02 14:11:11 +0400

    commit

---
+++ Goal Oriented Academy/Homeworks/day 72/homework/index.js
@@ -0,0 +1,26 @@
+
+let num1 = 10;
+let num2 = 5;
+
+
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+let quotient = num1 / num2;
+
+// Math methods
+let absoluteValue = Math.abs(-5);
+let roundedValue = Math.round(5.7); 
+let randomValue = Math.random(); 
+let maxNum = Math.max(10, 20, 30); 
+let minNum = Math.min(10, 20, 30); 
+
+console.log("Sum:", sum);
+console.log("Difference:", difference);
+console.log("Product:", product);
+console.log("Quotient:", quotient);
+console.log("Absolute Value:", absoluteValue);
+console.log("Rounded Value:", roundedValue);
+console.log("Random Value:", randomValue);
+console.log("Max Number:", maxNum);
+console.log("Min Number:", minNum);
