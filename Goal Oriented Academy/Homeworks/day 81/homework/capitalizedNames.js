5e0a899d3a572de09f26e1900d5f207a6c480a9a
Sandroz4 <sandrozabakhidze888@gmail.com>
2024-05-25 14:36:48 +0400

    commit

---
+++ Goal Oriented Academy/Homeworks/day 81/homework/capitalizedNames.js
@@ -0,0 +1,17 @@
+// // 3. You will be presented with a list of names. The task is to bring it back
+// // New list with all words in the capitalize option, use map.
+
+
+// const names = ['sandro', 'giorguna', 'jangira']
+
+
+// function capitalizeNames(names){
+//     return names.map(function(name){
+//         return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase() 
+//     });
+// }
+
+// let capitalizedNames = capitalizeNames(names)
+// console.log(capitalizedNames)
+
+
---
+++ Goal Oriented Academy/Homeworks/day 81/homework/manualfilter.js
@@ -0,0 +1,20 @@
+// function manualFilter(subFunc, arr) {
+//     const newArr = [];
+//     for (let i = 0; i < arr.length; i++) {
+//         if (subFunc(arr[i])) {
+//             newArr.push(arr[i]);
+//         }
+//     }
+//     return newArr;
+// }
+
+// function isEven(value) {
+//     return value % 2 === 0;
+// }
+
+// const result = manualFilter(isEven, [1, 2, 3, 4, 5]);
+// console.log(result); 
+
+
+
+
