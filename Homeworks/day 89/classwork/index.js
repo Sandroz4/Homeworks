6fb5a3d97d2294d3e37a15692029dba3aaa66808
Sandroz4 <sandrozabakhidze888@gmail.com>
2024-05-30 23:14:34 +0400

    commit

---
+++ Homeworks/day 89/classwork/index.js
@@ -0,0 +1,13 @@
+// node '.\Homeworks\day 89\classwork\index.js'
+
+// const numbers = [1,5,8,4,9,3,10];
+// const lastPrime = numbers.findLastIndex(function(number){
+//     let count = 0;
+//     for(let i = 0; i <= number; i++){
+//         if (count > 2) return false
+//         if(number % i == 0) count++
+//     }
+//     return true
+// })
+
+// console.log(lastPrime)
