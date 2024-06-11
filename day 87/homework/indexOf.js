b9b8bc3cfadf3b28322f8f71b440e90e5e835a45
Sandroz4 <sandrozabakhidze888@gmail.com>
2024-06-11 21:29:47 +0400

    commit

---
+++ day 87/homework/indexOf.js
@@ -0,0 +1,33 @@
+// // Index of First Occurrence of a Character: Find the index
+// //  of the first occurrence of a specific character in a string.
+
+// const name = ['sandro']
+
+// console.log((name[0]).indexOf('n'))
+
+
+
+// // Index of First Occurrence of a Number: Determine the index 
+// // of the first occurrence of a specific number in an array.
+
+// const numbers = [1, 2, 3, 4, 5, 6]
+
+// console.log(numbers.indexOf(4),)
+
+
+
+// // Index of First Occurrence of a Substring: Find the index 
+// // of the first occurrence of a specific substring in a string.
+
+
+// function indexOfSubstring(str, substring) {
+//     return str.indexOf(substring);
+// }
+
+
+// let string = "Hello Sandro!"
+// let substring = "Sandro"
+
+// console.log(indexOfSubstring(string, substring))
+
+
