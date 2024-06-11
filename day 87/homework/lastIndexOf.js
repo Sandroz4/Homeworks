78dcac15647686bdf213386adfd7d93aa8d64895
Sandroz4 <sandrozabakhidze888@gmail.com>
2024-06-11 21:47:05 +0400

    commit

---
+++ day 87/homework/lastIndexOf.js
@@ -0,0 +1,14 @@
+// // 1 Last Index of a Character in a Sentence: Find the last 
+// // index of a specific character in a given sentence.
+
+// function lastIndexOfCharacter(sentence, char) {
+//     return sentence.lastIndexOf(char)
+// }
+
+
+// let sentence = "Hello Sandro!"
+// let char = 'o'
+
+// console.log(lastIndexOfCharacter(sentence, char))
+
+
