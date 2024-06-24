64bed1c350ee25f385d1608c5f6a88d5fa842451
Sandroz4 <sandrozabakhidze888@gmail.com>
2024-06-24 22:30:40 +0400

    commit

--- day 91/homework/spread.js
+++ day 91/homework/spread.js
@@ -1,18 +1,38 @@
-// 1. Array Concatenation: Combine multiple arrays into a 
-// single array using the spread operator.
+// // 1. Array Concatenation: Combine multiple arrays into a 
+// // single array using the spread operator.
+// const array1 = [1, 2, 3]
+// const array2 = [4, 5, 6]
+// const array3 = [7, 8, 9]
+
+// const combinedArray = [...array1, ...array2, ...array3] 
+// console.log(combinedArray) 
 
 
-// 2. Object Merging: Merge properties from multiple objects
-//  into a new object using the spread operator.
+// // 2. Object Merging: Merge properties from multiple objects
+// //  into a new object using the spread operator.
+// const obj1 = { val1: 1, val2: 2 }
+// const obj2 = { val1: 3, val2: 4 }
+// const obj3 = { val1: 5 }
+
+// const mergedObject = { ...obj1, ...obj2, ...obj3 } 
+// console.log(mergedObject) 
+
+// // 3. Array Modification: Add new elements to an array or modify 
+// // existing elements using the spread operator.
+// const originalArray = [1, 2, 3];
+// const newArray = [...originalArray, 4, 5] 
+// console.log(newArray) 
 
 
-// 3. Array Modification: Add new elements to an array or modify 
-// existing elements using the spread operator.
+// // 4. Copying Arrays: Create a copy of an array using the spread
+// //  operator.
+// const arrayToCopy = [1, 2, 3] 
+// const copiedArray = [...arrayToCopy] 
+// console.log(copiedArray) 
 
 
-// 4. Copying Arrays: Create a copy of an array using the spread
-//  operator.
-
-
-// 5. String to Array Conversion: Convert a string into an array 
-// of characters using the spread operator.
+// // 5. String to Array Conversion: Convert a string into an array 
+// // of characters using the spread operator.
+// const string = "hello"
+// const charArray = [...string]
+// console.log(charArray)
