c21705cfc5f175625915d7cf13f9c2f226d8ca42
Sandroz4 <sandrozabakhidze888@gmail.com>
2024-06-25 20:20:40 +0400

    commit

--- day 92/classwork/cw1/script.js
+++ day 92/classwork/cw1/script.js
@@ -1,29 +1,43 @@
-class Person {
-    constructor(firstName, lastName, email) {
-        this.firstName = firstName
-        this.lastName = lastName
-        this.email = email
-    }
-}
+// const signUpForm = document.getElementById('signup');
+// const signInForm = document.getElementById('signin');
 
-const dataBase = [];
+// const dataBase = [];
 
-function updateDatabaseOutput() {
-    const databaseOutput = document.getElementById('databaseOutput')
-    databaseOutput.textContent = JSON.stringify(dataBase, null, 2)
-}
+// class Account {
+//     #password;
 
+//     constructor(email, password) {
+//         this.email = email;
+//         this.#password = password;  
+//     }
 
-document.getElementById('dataForm').addEventListener('submit', function(event) {
-    event.preventDefault()
+//     get password() {
+//         return this.#password;
+//     }
+// }
 
-    const firstName = document.getElementById('name').value
-    const lastName = document.getElementById('lastname').value
-    const email = document.getElementById('email').value
-    const newPerson = new Person(firstName, lastName, email)
+// signUpForm.addEventListener('submit', (e) => {
+//     e.preventDefault();
 
+//     const email = signUpForm.elements['email'].value;
+//     const password = signUpForm.elements['password'].value;
 
-    dataBase.push(newPerson)
-    updateDatabaseOutput()
-    document.getElementById('dataForm').reset()
-});
+//     dataBase.push(new Account(email, password));
+
+//     alert('Account successfully created');
+// });
+
+// signInForm.addEventListener('submit', (e) => {
+//     e.preventDefault();
+
+//     const email = signInForm.elements['email'].value;
+//     const password = signInForm.elements['password'].value;
+
+//     const acc = dataBase.find((obj) => obj.email === email);
+
+//     if (acc && acc.password === password) {
+//         alert('Successfully signed in');
+//     } else {
+//         alert('Invalid email or password');
+//     }
+// });
---
+++ day 92/homework/hw0/script.js
@@ -0,0 +1,29 @@
+class Person {
+    constructor(firstName, lastName, email) {
+        this.firstName = firstName
+        this.lastName = lastName
+        this.email = email
+    }
+}
+
+const dataBase = [];
+
+function updateDatabaseOutput() {
+    const databaseOutput = document.getElementById('databaseOutput')
+    databaseOutput.textContent = JSON.stringify(dataBase, null, 2)
+}
+
+
+document.getElementById('dataForm').addEventListener('submit', function(event) {
+    event.preventDefault()
+
+    const firstName = document.getElementById('name').value
+    const lastName = document.getElementById('lastname').value
+    const email = document.getElementById('email').value
+    const newPerson = new Person(firstName, lastName, email)
+
+
+    dataBase.push(newPerson)
+    updateDatabaseOutput()
+    document.getElementById('dataForm').reset()
+});
