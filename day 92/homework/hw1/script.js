4cb8efd4f30947d97df798d9017f13eb256bec31
Sandroz4 <sandrozabakhidze888@gmail.com>
2024-06-26 18:44:37 +0400

    commit

---
+++ day 92/homework/hw1/script.js
@@ -0,0 +1,78 @@
+// // 1. Define a class called Person with properties like name, age, and gender.
+
+// class Person{
+//     constructor(name, age, gender){
+//         this.name = name;
+//         this.age = age;
+//         this.gender = gender
+//     }
+// }
+
+// 2. Create an object, using the Person class and assign it to a variable.
+
+// class Person{
+//     constructor(name, age, gender){
+//         this.name = name;
+//         this.age = age;
+//         this.gender = gender
+//     }
+// }
+
+// const person2 = new Person('sandro', 16, 'male')
+
+
+// // 3. Add methods to the Person class such as greet() to log a greeting.
+
+// class Person{
+//     constructor(name, age, gender){
+//         this.name = name;
+//         this.age = age;
+//         this.gender = gender
+//     }
+
+//     greet(){
+//         console.log('hello' + this.name)
+//     }
+// }
+
+// const person2 = new Person('sandro', 16, 'male')
+// person2.greet()
+
+// 4. Access and log the properties of an object created in task2.
+
+// class Person{
+//     constructor(name, age, gender){
+//         this.name = name;
+//         this.age = age;
+//         this.gender = gender
+//     }
+// }
+
+// const person2 = new Person('sandro', 16, 'male')
+
+// console.log(person2.name)
+// console.log(person2.age)
+// console.log(person2.gender)
+
+
+
+
+
+
+// 5. Update the values of class properties in a new object.
+
+
+// 6. Create multiple objects, using the Person class and assign them to variables.
+
+
+// 7. Define a getter method in a class that returns a specific property value.
+
+
+// 8. Use a getter method to create a computed property that generates its value
+//  based on certain conditions or calculations.
+
+
+// 9. Define a setter method in a class that sets the value of a specific property.
+
+
+// 10. Define a private property inside a class using the # prefix.
