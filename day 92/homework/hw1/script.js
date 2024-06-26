5edb74e199f9b54e07d00a3c34ede02fc6d80be0
Sandroz4 <sandrozabakhidze888@gmail.com>
2024-06-26 21:32:45 +0400

    commit

--- day 92/homework/hw1/script.js
+++ day 92/homework/hw1/script.js
@@ -62,17 +62,118 @@
 // 5. Update the values of class properties in a new object.
 
 
+// class Person{
+//     constructor(name, age, gender){
+//         this.name = name;
+//         this.age = age;
+//         this.gender = gender
+//     }
+// }
+
+// const person2 = new Person('sandro', 16, 'male')
+
+// console.log(person2.name)
+// console.log(person2.age)
+// console.log(person2.gender)
+
+
+
+
+
 // 6. Create multiple objects, using the Person class and assign them to variables.
 
+// class Person{
+//     constructor(name, age, gender){
+//         this.name = name;
+//         this.age = age;
+//         this.gender = gender
+//     }
+// }
 
-// 7. Define a getter method in a class that returns a specific property value.
+// const person2 = new Person('sandro', 16, 'male')
+// const person2 = new person('luka', 17, 'male')
+
+// // 7. Define a getter method in a class that returns a specific property value.
+
+// class Person{
+//     constructor(name, age, gender){
+//         this.name = name;
+//         this.age = age;
+//         this.gender = gender
+//     }
+
+//     get(){
+//         return this.name
+//     }
+// }
+
+// const person2 = new Person('sandro', 16, 'male')
+// const person3 = new person('luka', 17, 'male')
+
+// // 8. Use a getter method to create a computed property that generates its value
+// //  based on certain conditions or calculations.
+
+// class Person {
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+
+ 
+//     get canVote() {
+//         return this.age >= 18;  
+//     }
+// }
+
+ 
+// const person1 = new Person("John", 25);
+
+ 
 
 
-// 8. Use a getter method to create a computed property that generates its value
-//  based on certain conditions or calculations.
+
+// // 9. Define a setter method in a class that sets the value of a specific property.
+
+// class Person {
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//         this.height = 0; 
+//     }
+
+//     set height(value) {
+//         if (value > 0) {
+//             this.height = value; 
+//         } else {
+//             console.log("Height must be a positive number.");
+//         }
+//     }
+
+ 
+//     get height() {
+//         return this.height;
+//     }
+// }
 
 
-// 9. Define a setter method in a class that sets the value of a specific property.
+// const person1 = new Person("Jane", 30);
 
 
 // 10. Define a private property inside a class using the # prefix.
+
+// class Account{
+//     #password;
+
+//     constructor(password, lastname){
+//         this.#password = password;
+//         this.lastname = lastname;
+//     }
+
+//     get password(){
+//         return this.#password
+//     }
+// }
+
+// const person1 = new Account('luka123', 'luka')
+
+// console.log(person1.password)
