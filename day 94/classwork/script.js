d220f5094d30c9753011543ecd1e6369be278e59
Sandroz4 <sandrozabakhidze888@gmail.com>
2024-07-01 23:53:50 +0400

    commit

--- day 94/classwork/script.js
+++ day 94/classwork/script.js
@@ -98,3 +98,40 @@
 // }, (rejected)=>{
 //     console.log(rejected)
 // })
+
+
+
+// function walkDog() {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve('you walk the dog');
+//         }, 1500);
+//     });
+// }
+
+// function cleanKitchen() {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve('you clean the kitchen');
+//         }, 2500);
+//     });
+// }
+
+// function takeOutTrash() {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve('you take out the trash');
+//         }, 500);
+//     });
+// }
+
+// walkDog()
+//     .then(value => {
+//         console.log(value);
+//         return cleanKitchen();
+//     })
+//     .then(value => {
+//         console.log(value);
+//         return takeOutTrash();
+//     })
+//     .then(value => console.log(value));
