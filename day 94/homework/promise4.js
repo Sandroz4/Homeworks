120ea7f374084e7baed0894fa2b1755dff4e77d0
Sandroz4 <sandrozabakhidze888@gmail.com>
2024-07-01 22:32:03 +0400

    commit

--- day 94/homework/promise4.js
+++ day 94/homework/promise4.js
@@ -1,2 +1,13 @@
 // 4. Use multiple .then callbacks to handle 
 // different stages of promise resolution.
+
+
+const rejectingPromise = new Promise((_, reject) => {
+    setTimeout(() => {
+        reject(new Error("error"));
+    }, 1000);
+});
+
+rejectingPromise.catch((error) => {
+    console.error(error.message);  
+});
