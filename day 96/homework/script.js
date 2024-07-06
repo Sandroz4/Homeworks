e1b658bd482b32dff666cec8e181a12e65a4320e
Sandroz4 <sandrozabakhidze888@gmail.com>
2024-07-07 02:05:09 +0400

    commit

--- day 96/homework/script.js
+++ day 96/homework/script.js
@@ -28,13 +28,56 @@
 
 
 
-// 3. Create a Promise that resolves with a number (e.g., 5). Chain a
-//  .then handler to double the number and return it, then chain another 
-//  .then handler to log the doubled number.
+// // 3. Create a Promise that resolves with a number (e.g., 5). Chain a
+// //  .then handler to double the number and return it, then chain another 
+// //  .then handler to log the doubled number.
 
-// 4. Create a function that returns a Promise which resolves after 2 
-// seconds with a message "First". Chain another then that waits 1 second 
-// before logging "Second".
+ 
+// const promise = new Promise((resolve) => {
+//     resolve(5);  
+//   });
+  
+//   promise
+//     .then((number) => {
+//       return number * 2 
+//     })
+//     .then((doubledNumber) => {
+//       console.log("doubled number:", doubledNumber) 
+//     });
+  
+
+
+
+// // 4. Create a function that returns a Promise which resolves after 2 
+// // seconds with a message "First". Chain another then that waits 1 second 
+// // before logging "Second".
+
+// function delayedPromise(){
+//     return new Promise((resolve)=>{
+//         setTimeout(()=>{
+//             resolve('first')
+//         }, 2000)
+//     })
+// }
+
+
+// delayedPromise().then((message) => {
+//     return new Promise((resolve)=>{
+//         setTimeout(()=>{
+//             resolve('second')
+//         }, 1000)
+//     })
+// })
+// .then((message)=>{
+//     console.log(message)
+// })
+
+
+
+
+
+
+
 
 // 5. Create a function that returns a Promise which rejects after 2 seconds
 //  with an error message "Task 3 failed". Handle the rejection with a .catch 
