9269eaf2355ab9ff180560dbc02d685d23ece151
Sandro Zabakhidze <145058114+Sandroz4@users.noreply.github.com>
2024-12-13 21:28:53 +0400

    commit

---
+++ day 026/homework/tasks.py
@@ -0,0 +1,52 @@
+# def manual_sum(list):              
+#     sum = 0
+#     for i in list:
+#         sum += i
+#     return sum
+
+# print(manual_sum([1,2,3,4,5,6,7,8,9,10]))
+
+
+# def manual_max(list):
+#     max_number = list[0]  
+#     for i in list:
+#         if max_number < i:
+#             max_number = i
+#     return max_number
+
+
+# print(manual_max([1,2,2,2,23,25,7,1,62,124,1,6]))
+
+
+# def manual_min(list):
+#     minimal_number = list[0] 
+#     for i in list:
+#         if minimal_number > i:
+#             minimal_number = i
+#     return minimal_number
+
+# print(manual_min([3,5,2,37,3,2,2,4,1,9]))
+
+
+# def manual_len(list):
+#     count = 0
+#     for i in list:
+#         count += 1
+#     return count
+
+# print(manual_len([7,3,2,363,12,4]))
+
+
+# def manual_reduce(list):
+#     copied_list = []
+#     for i in list:
+#         copied_list.append(i)
+#     list += "]"
+#     list += "["
+#     for i in copied_list:
+#         list.append(i)
+#     return list 
+
+# print(manual_reduce([10, 20, 30]))
+
+    
